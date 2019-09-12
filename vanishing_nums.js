ccc5c7dc614cf36475a8524ef21c306c2e9dfe38
Anshuj <anshuj@gmail.com>
2019-09-12 16:45:41 -0700

    Adds reset button to it

--- vanishing_nums.js
+++ vanishing_nums.js
@@ -85,23 +85,10 @@ const draw = function() {
     txt = parseInt(randomIn10(), 10); //pick a random num between 0 - 9
   }
   if (j > height_arr[col] - SQR_SIZE) {
-    console.log("GAME OVER!");
     draw_text_in_sqr("GAME OVER!", MAX_X / 2 - 100, MAX_Y / 2, "#FF0000");
     clearInterval(myVar);
   }
 
-  // console.log(
-  //   "New j: ",
-  //   j,
-  //   "i: ",
-  //   i,
-  //   "col",
-  //   col,
-  //   "height_arr[",
-  //   col,
-  //   "]: ",
-  //   height_arr[col]
-  // );
   j += DELTA;
 };
 
@@ -110,7 +97,6 @@ canvas.addEventListener(
   function(e) {
     let x = e.clientX;
     let y = e.clientY;
-    console.log("x: ", x, "y: ", y);
     //We hit the play button
     if (x > 930 && x < 965 && y >= 200 && y < 280) {
       if (paused) {
@@ -119,14 +105,30 @@ canvas.addEventListener(
         //Change play to pause button
         draw_rect(840, 100, 5, 50, BACKGROUND_COLOR); //, "#FF0000");
         draw_rect(860, 100, 5, 50, BACKGROUND_COLOR); //, "#FF0000");
+        draw_circle();
         myVar = setInterval(draw, 2);
         paused = 0;
       } else {
         draw_rect(800, 0, 100, 600, PANEL_COLOR);
         draw_triangle();
+        draw_circle();
         clearInterval(myVar);
         paused = 1;
       }
+    } else if (x > 920 && x < 980 && y >= 300 && y < 420) {
+      draw_rect(0, 0, 800, 600);
+      for (let m = 0; m < TOTAL_COLS; m++) {
+        height_arr[m] = 600;
+      }
+      for (let n = 0; n < TOTAL_COLS; n++) {
+        column[n] = [];
+      }
+      draw_rect(800, 0, 100, 600, PANEL_COLOR);
+      draw_triangle();
+      draw_circle();
+      clearInterval(myVar);
+      res = generateEquations();
+      paused = 1;
     } else {
       let x = findOrigin(e.clientX);
       let y = findOrigin(e.clientY);
