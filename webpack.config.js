8d5561c0b5e3a318799c100003b10a63c74c0aea
Anshuj <anshuj@gmail.com>
2019-09-13 10:44:43 -0700

    Refactored the code to deploy to git pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./vanishing_nums.js",
+  entry: "./dist/vanishing_nums.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, ".")
